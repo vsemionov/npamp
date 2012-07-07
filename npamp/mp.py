905d2cde28717b22edc5a0419f25ddc2bed3a2e9
Victor Semionov <vsemionov@gmail.com>
2012-07-07 16:14:25 +0300

    wording

--- npamp/mp.py
+++ npamp/mp.py
@@ -39,7 +39,7 @@ import core
 
 
 # The MP IO subsystem is flawed and needs to be redesigned.
-# It contains many concurrency issues (race conditions) and is not 100% reliable.
+# It contains some concurrency issues (race conditions on errors) and is not completely reliable.
 # As a workaround for any eventual issues, use the command-line interface (as opposed to the GUI).
 
 
