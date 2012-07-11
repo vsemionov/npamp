1c5b242b1a5d7b1729d1b6897dff33420bc32b17
Victor Semionov <vsemionov@gmail.com>
2012-07-12 01:04:16 +0300

    experimental: do not account for fluence rtol (it's included in amplification rtol), neither for the min. number of discretization steps, nor for the error in the results

--- npamp/params.py
+++ npamp/params.py
@@ -88,8 +88,7 @@ ext_opt_geom_beamradius = (beam_radius / 2.0, beam_radius * 2.0)
 ext_opt_geom_resolution = (24, 24)
 
 time_trunc_rtol = 1.0e-2
-fluence_rtol = 1.0e-2
-energy_rtol = 1.0e-2
+int_rtol = 1.0e-2
 amp_rtol = 2.0e-2
 
 min_count_rho = 0
