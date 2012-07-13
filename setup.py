d8d0a4c74ff169ebca86804d2288dea0926a6e14
Victor Semionov <vsemionov@gmail.com>
2012-07-14 02:33:15 +0300

    unconditional

--- setup.py
+++ setup.py
@@ -22,35 +22,35 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 
-import sys
-import os
-
-
 # py2exe winshell fix
 # see http://www.py2exe.org/index.cgi/WinShell
-if os.name == "nt":
-    # ModuleFinder can't handle runtime changes to __path__, but win32com uses them
+# ...
+# ModuleFinder can't handle runtime changes to __path__, but win32com uses them
+try:
+    # py2exe 0.6.4 introduced a replacement modulefinder.
+    # This means we have to add package paths there, not to the built-in
+    # one.  If this new modulefinder gets integrated into Python, then
+    # we might be able to revert this some day.
+    # if this doesn't work, try import modulefinder
     try:
-        # py2exe 0.6.4 introduced a replacement modulefinder.
-        # This means we have to add package paths there, not to the built-in
-        # one.  If this new modulefinder gets integrated into Python, then
-        # we might be able to revert this some day.
-        # if this doesn't work, try import modulefinder
-        try:
-            import py2exe.mf as modulefinder
-        except ImportError:
-            import modulefinder
-        import win32com, sys
-        for p in win32com.__path__[1:]:
-            modulefinder.AddPackagePath("win32com", p)
-        for extra in ["win32com.shell"]: #,"win32com.mapi"
-            __import__(extra)
-            m = sys.modules[extra]
-            for p in m.__path__[1:]:
-                modulefinder.AddPackagePath(extra, p)
+        import py2exe.mf as modulefinder
     except ImportError:
-        # no build path setup, no worries.
-        pass
+        import modulefinder
+    import win32com, sys
+    for p in win32com.__path__[1:]:
+        modulefinder.AddPackagePath("win32com", p)
+    for extra in ["win32com.shell"]: #,"win32com.mapi"
+        __import__(extra)
+        m = sys.modules[extra]
+        for p in m.__path__[1:]:
+            modulefinder.AddPackagePath(extra, p)
+except ImportError:
+    # no build path setup, no worries.
+    pass
+
+
+import sys
+import os
 
 
 from distutils.core import setup
