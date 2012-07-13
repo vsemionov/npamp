2425640838c7bcd56b1ce9dcb6e931e29de49298
Victor Semionov <vsemionov@gmail.com>
2012-07-13 12:07:20 +0300

    refactoring

--- examples/sawtooth_pulse.py
+++ examples/sawtooth_pulse.py
@@ -7,19 +7,15 @@ import model
 
 class SawtoothPulse(model.pulse.SinglePulse):
     
-    def __init__(self, *args, **kwargs):
-        super(SawtoothPulse, self).__init__(*args, **kwargs)
-        self.slope = self.ref_density / self.duration
-    
     def density(self, t):
         t -= self.t0
-        return self.slope * t if 0.0 <= t <= self.duration else 0.0
+        return (self.ref_density / self.duration) * t if 0.0 <= t <= self.duration else 0.0
     
     def density_integral(self, t):
         t -= self.t0
         t = max(t, 0.0)
         t = min(t, self.duration)
-        return self.slope * t**2.0 / 2.0
+        return (self.ref_density / self.duration) * t**2.0 / 2.0
 
 
-setattr(model.pulse, SawtoothPulse.__name__, SawtoothPulse)
+model.pulse.SawtoothPulse = SawtoothPulse
