45a911f7090a742a6b27d9bcc8641c53eb92ed1f
Victor Semionov <vsemionov@gmail.com>
2012-07-14 02:19:02 +0300

    py2exe winshell fix

--- setup.py
+++ setup.py
@@ -25,6 +25,34 @@
 import sys
 import os
 
+
+# py2exe winshell fix
+# see http://www.py2exe.org/index.cgi/WinShell
+if os.name == "nt":
+    # ModuleFinder can't handle runtime changes to __path__, but win32com uses them
+    try:
+        # py2exe 0.6.4 introduced a replacement modulefinder.
+        # This means we have to add package paths there, not to the built-in
+        # one.  If this new modulefinder gets integrated into Python, then
+        # we might be able to revert this some day.
+        # if this doesn't work, try import modulefinder
+        try:
+            import py2exe.mf as modulefinder
+        except ImportError:
+            import modulefinder
+        import win32com, sys
+        for p in win32com.__path__[1:]:
+            modulefinder.AddPackagePath("win32com", p)
+        for extra in ["win32com.shell"]: #,"win32com.mapi"
+            __import__(extra)
+            m = sys.modules[extra]
+            for p in m.__path__[1:]:
+                modulefinder.AddPackagePath(extra, p)
+    except ImportError:
+        # no build path setup, no worries.
+        pass
+
+
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
